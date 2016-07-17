78c797eaa2a868c6e1fd2c1f61c5071bb6e43330
teebu <junkbox0@gmail.com>
2016-07-17 11:46:40 -0700

    auto detect ip based on network card passed

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var Cap = require('cap').Cap,
     decoders = require('cap').decoders,
     PROTOCOL = decoders.PROTOCOL
 
-var stream = require('stream');
-var _ = require('underscore');
+var stream = require('stream'),
+    _ = require('underscore'),
+    interfaceAddresses = require('interface-addresses'),
+    addresses = interfaceAddresses()
+
 var hex_to_int_array = require('./helpers.js').hex_to_int_array;
 var int_array_to_hex = require('./helpers.js').int_array_to_hex;
 
@@ -13,7 +16,7 @@ var int_array_to_hex = require('./helpers.js').int_array_to_hex;
 var create_session = function (arp_interface) {
     try {
           var c = new Cap(),
-          device = Cap.findDevice('192.168.1.2'),
+          device = (arp_interface) ? Cap.findDevice(addresses[arp_interface]) : Cap.findDevice(addresses['Ethernet']), // in windows it uses the name of the network card
           filter = 'arp',
           bufSize = 10 * 1024 * 1024,
           buffer = new Buffer(65535);
@@ -49,18 +52,8 @@ var register = function(mac_addresses, arp_interface, timeout) {
     mac_addresses.forEach(function(mac_address){
         just_emitted[mac_address] = false;
     });
-    //pcap_session.on('packet', function(raw_packet) {
-    cap_info.session.on('packet', function(nbytes, trunc) {
-        var packet;
 
-        /**
-         * Perform a try/catch on packet decoding until pcap
-         * offers a non-throwing mechanism to listen for errors
-         * (We're just ignoring these errors because TCP packets with an
-         *  unknown offset should have no impact on this application) 
-         *
-         * See https://github.com/mranney/node_pcap/issues/153
-         */
+    cap_info.session.on('packet', function(nbytes, trunc) {
 
         if (cap_info.linkType === 'ETHERNET') {
             var ret = decoders.Ethernet(cap_info.buffer);
@@ -68,19 +61,17 @@ var register = function(mac_addresses, arp_interface, timeout) {
             if (ret.info.type === PROTOCOL.ETHERNET.ARP) {
                 //console.log('Decoding ARP ...');
                 //console.log('ret', ret.info.srcmac);
-            }
-        }
 
-        if(ret.info.type === PROTOCOL.ETHERNET.ARP) { //ensures it is an arp packet
-            //for element in the mac addresses array
-            mac_addresses.forEach(function(mac_address){
-                if(!just_emitted[mac_address] && 
-                    _.isEqual(ret.info.srcmac, mac_address)) {
-                    readStream.emit('detected', mac_address);
-                    just_emitted[mac_address] = true;
-                    setTimeout(function () { just_emitted[mac_address] = false; }, timeout);
-                }                
-            });
+                //for element in the mac addresses array
+                mac_addresses.forEach(function (mac_address) {
+                    if (!just_emitted[mac_address] &&
+                      _.isEqual(ret.info.srcmac, mac_address)) {
+                        readStream.emit('detected', mac_address);
+                        just_emitted[mac_address] = true;
+                        setTimeout(function () { just_emitted[mac_address] = false; }, timeout);
+                    }
+                });
+            }
         }
     });
     return readStream;
